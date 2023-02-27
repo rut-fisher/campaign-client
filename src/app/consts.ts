5e931988062a9c6beaf1916c9b3d273dfb0ab530
rut_fisher <r0504177108@gmail.com>
2023-02-27 13:17:13 +0200

    groups height

--- src/app/consts.ts
+++ src/app/consts.ts
@@ -7,6 +7,6 @@ export const SUB_COLOR = "#012d45";
 export const THIRD_COLOR = "#eaae20"
 export const ANIMATION_DELAY_BETWWEN_GROUP_ENTER = 200;
 export const NUM_OF_GROUPS_TO_DISPLAY = 6;
-export const NUM_OF_PERSONALS_TO_DISPLAY = 9;
+export const NUM_OF_PERSONALS_TO_DISPLAY = 12;
 export const INTERVAL_OF_REPLACE = 5000;
 export const DISPLAY_GROUPS = false;
